2be76248dcd2d64a71a6fa603f2be2f387f28553
Leeviana Gray <leeviana@gmail.com>
2015-01-28 00:13:46 -0500

    page that displays user data in database

---
+++ app/controllers/Users.scala
@@ -0,0 +1,43 @@
+package controllers
+
+import models._
+import play.api._
+import play.api.mvc._
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+
+import play.modules.reactivemongo._
+import scala.concurrent.Future
+
+import reactivemongo.api._
+import reactivemongo.api.collections.default.BSONCollection
+import reactivemongo.bson._
+
+import play.modules.reactivemongo.MongoController
+
+/**
+ * @author Leevi
+ */
+object Users extends Controller with MongoController {
+    
+    val collection = db[BSONCollection]("users")
+    //ReactiveMongoPlugin.db.collection[BSONCollection]("users")
+    
+    //def collection: BSONCollection = db.collection[BSONCollection]("users")
+    
+    def index = Action.async { implicit request =>
+        //implicit val reader = User.UserReader
+        
+                
+             val query = BSONDocument(
+            "$query" -> BSONDocument())
+        
+            val activeSort = request.queryString.get("sort").flatMap(_.headOption).getOrElse("none")
+            val found = collection.find(query).cursor[User]
+            
+            found.collect[List]().map { users =>
+                //Ok(views.html.users(users, activeSort))
+                Ok(views.html.index(users))
+            }
+        
+    }
+}
--- app/models/user.scala
+++ app/models/user.scala
@@ -1,5 +1,10 @@
 package models
 
+import play.api.data.Forms._
+import play.api.data._
+import play.api.data.validation.Constraints._
+import play.api.data.format.Formats._
+
 import reactivemongo.bson._
 
 /**
@@ -8,7 +13,7 @@ import reactivemongo.bson._
 case class User (
     id: BSONObjectID,
     username: String,
-    subscriptions: List[BSONObjectID], // list of calIDs
+    subscriptions: BSONArray, // list of calIDs
     settings: UserSettings // settings parameters
 )
 
@@ -18,16 +23,30 @@ object User {
             User(
                 doc.getAs[BSONObjectID]("_id").get,
                 doc.getAs[String]("username").get,
-                doc.getAs[List[BSONObjectID]]("subscriptions").get,
+                // doc.getAs[List[BSONObjectID]]("subscriptions").get,
+                doc.getAs[BSONArray]("subscriptions").get,
                 doc.getAs[UserSettings]("settings").get)
         }
     }
     
     implicit object UserWriter extends BSONDocumentWriter[User] {
         def write(user: User): BSONDocument = BSONDocument(
-            "id" -> user.id,
+            "_id" -> user.id,
             "username" -> user.username,
             "subscriptions" -> user.subscriptions,
             "settings" -> user.settings)
     }
+      
+    val form = Form(
+        mapping(
+            // "id" -> text verifying pattern(
+            //    """[a-fA-F0-9]{24}""".r,
+            //    "constraint.objectId",
+            //    "error.objectId"),
+            "id" -> ignored(BSONObjectID.generate),
+            "username" -> nonEmptyText,
+            "subscriptions" -> ignored(BSONArray.empty),
+            "settings" -> ignored(UserSettings()) 
+        ) (User.apply) (User.unapply) 
+    )
 }
