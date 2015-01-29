6b814f3c1231339e497534e16c0e89cd322f972c
Susan Zhang <sz56@duke.edu>
2015-01-28 23:37:46 -0500

    Fixed merge conflict

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,14 +5,9 @@ import play.api.mvc._
 
 object Application extends Controller {
 
-<<<<<<< HEAD
-  def index = Action {
-    Ok(views.html.index("Test 123"))
-  }
-=======
 //  def index = Action {
 //    Ok(views.html.index("Your new application is ready."))
 //  }
->>>>>>> master
+
 
 }
