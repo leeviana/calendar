8439e2c121b482ccb614145813de48173e71498e
Leeviana Gray <leeviana@gmail.com>
2015-02-17 20:49:47 -0500

    some view niceties

--- app/models/group.scala
+++ app/models/group.scala
@@ -15,7 +15,7 @@ case class Group(
     _id: BSONObjectID = BSONObjectID.generate,
     name: String,
     owner: BSONObjectID, // foreign ref
-    userIDs: BSONArray // List[BSONObjectID]
+    userIDs: List[BSONObjectID] // List[BSONObjectID]
     )
 
 object Group {
@@ -25,19 +25,19 @@ object Group {
         mapping(
             "_id" -> ignored(BSONObjectID.generate),
             "name" -> nonEmptyText,
-            "owner" -> nonEmptyText,
-            "userIDs" -> ignored(BSONArray.empty)) { (id, name, owner, userIDs) =>
+            "owner" -> nonEmptyText
+            ) { (id, name, owner) =>
                 val ownID = BSONObjectID.apply(owner)
+                
                 Group(
                     id,
                     name,
                     ownID,
-                    userIDs.add(ownID))
+                    List[BSONObjectID](ownID))
             } { group =>
                 Some((
                     group._id,
                     group.name,
-                    group.owner.toString(),
-                    group.userIDs))
+                    group.owner.toString()))
             })
 }
